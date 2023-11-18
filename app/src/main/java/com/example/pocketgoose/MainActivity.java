cc52031616137fc7f63decd5bb531644e2a31cff
Angelina <114817414+LikaKlen@users.noreply.github.com>
2023-11-18 11:01:33 +0300

    upgrade3

--- app/src/main/java/com/example/pocketgoose/MainActivity.java
+++ app/src/main/java/com/example/pocketgoose/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         buttonExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finishAndRemoveTask();
+                finish();
             }
         });
         //кнопка старта
